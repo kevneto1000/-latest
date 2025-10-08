95395fe91f2ae682b4288f1e3fa62305f1433075
neto <netodboss@gmail.com>
2025-10-08 13:53:53 +0100

    complete

--- src/components/PaystackPayment.jsx
+++ src/components/PaystackPayment.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { PaystackButton } from 'react-paystack'
 
 function PaystackPayment() {
+  const publicKey = "pk_test_99d8f5a23465707c659bcd34e4658cd6acddaffe";
+  const email = "netodboss@gmail.com";
+  const amount = 5000 * 100;
+  const reference = new Date().getTime().toString();
+
+
+  const componentProps = {
+    email,
+    amount,
+    publicKey,
+    reference,
+
+    text: "Pay with Paystack (demo)",
+    onSuccess: (response) => {
+      alert("Payment Successful\nReference: " + response.reference);
+    },
+    onClose: () => {
+      alert("Payment unseuccessful");
+    },
+  }
+
   return (
-    <div>PaystackPayment</div>
+    <div className='p-3'>
+      <h3>Demo Paystack Payment</h3>
+      <PaystackButton className='btn btn-success' {...componentProps} />
+    </div>
   )
 }
 
